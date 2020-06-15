c9ad88df6cbdbfdc0ab1535f63c3d6e0f7992bc0
William Lian <me@williamlian.com>
2020-06-15 12:52:37 -0700

    do not wait for serial

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -26,9 +26,6 @@ activity::WebClient webClient;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
-  }
   for (int i = 0; i < BUTTON_COUNT; i++) {
     pinMode(i, INPUT_PULLUP);
     pinMode(LED_BASE_PIN + i, OUTPUT);
